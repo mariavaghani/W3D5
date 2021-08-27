d252b70f72b85c9a36b4583b49869b13872da0d0
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-08-27 16:56:02 -0400

    finished new_moves_positions

--- knights_travails/knights_travails.rb
+++ knights_travails/knights_travails.rb
@@ -1,6 +1,7 @@
 require_relative 'polytree'
 
 class KnightPathFinder
+  attr_reader :root_node
   
   def initialize(pos)
     @root_node = PolyTreeNode.new(pos)
@@ -27,25 +28,31 @@ class KnightPathFinder
   end
 
   def new_move_positions(pos)
-    KnightPathFinder.valid_moves(pos).reject { |moves| @considered_positions.include?(moves)}
+    return_moves = KnightPathFinder.valid_moves(pos).reject { |moves| @considered_positions.include?(moves)}
+    @considered_positions += return_moves
+    return_moves
   end
 
   def build_move_tree
-    new_moves_arr = self.new_move_positions(@root_node.value)
 
-    # queue = [self]
+    queue = [@root_node]
+
+    until queue.empty?
+      node = queue.shift
+      new_moves_arr = new_move_positions(node.value)
+      new_moves_arr.each { |move| node.add_child(PolyTreeNode.new(move)) }
+      node.children.each { |child| queue << child }
+    end
+
+
+# queue = [self]
     # until queue.empty?
     #   node = queue.shift
     #   return node if node.value == target_value
     #   node.children.each { |child| queue << child } 
     # end
 
-    until new_moves_arr.empty?
-      node = new_moves_arr.shift
-      new_moves_arr.each { |move| node.add_child(PolyTreeNode.new(move)) }
-
-
-    end
+    
   end
 
 end
@@ -58,6 +65,6 @@ if __FILE__ == $PROGRAM_NAME
   # p kpf.new_move_positions([0,0])
 
   kpf.build_move_tree
-  p kpf
+  p kpf.root_node.children[0].children[0]
 
 end

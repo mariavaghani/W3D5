93f424268e8cba7824276a054ba3cd2e4c3586d3
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-08-27 14:09:38 -0400

    add add_child and remove_child methods

--- poly_tree/vaghani_berzon_polytree/lib/00_tree_node.rb
+++ poly_tree/vaghani_berzon_polytree/lib/00_tree_node.rb
@@ -8,8 +8,28 @@ class PolyTreeNode
   end
 
   def parent=(parent_to_add)
-    return nil unless parent_to_add
+    @parent.children.delete_if { |child| child == self} if parent
     @parent = parent_to_add
-    @parent.children << self unless parent.children.include?(self) || parent == nil
+    if parent != nil
+      @parent.children << self unless parent.children.include?(self)
+    end
   end
+
+  def add_child(child_node)
+    child_node.parent = self
+    @children << child_node unless children.include?(child_node)
+  end
+
+  def remove_child(child_node)
+    raise "child is not a node" unless children.include?(child_node)
+    child_node.parent = nil 
+  end
+
+
+
+
+
+
+
+
 end

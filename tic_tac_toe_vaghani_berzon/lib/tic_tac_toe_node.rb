06b5955b3b6009cd29267cc9e4da1172d841317f
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-08-30 11:36:28 -0400

    working on children

--- tic_tac_toe_vaghani_berzon/lib/tic_tac_toe_node.rb
+++ tic_tac_toe_vaghani_berzon/lib/tic_tac_toe_node.rb
@@ -1,12 +1,13 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board, :next_mover_mark, :next_mover_pos 
+  attr_reader :board, :next_mover_mark, :next_mover_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
     @prev_move_pos = prev_move_pos
     @children = []
+    
   end
 
 
@@ -19,15 +20,27 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    # children = []
     
-    board.each_with_index do |row, row_idx|
+    board.rows.each_with_index do |row, row_idx|
       row.each_with_index do |ele, col_idx|
-        if board.empty?(row_idx, col_idx)
+        if board.empty?([row_idx, col_idx])
           copy = board.dup
           copy[[row_idx, col_idx]] = next_mover_mark
-          @children << TicTacToeNode.new(copy, next_mover_mark)
+          next_next_mover_mark = next_mover_mark == :x ? :o : :x
+          children << TicTacToeNode.new(copy, next_next_mover_mark, prev_move_pos = [row_idx, col_idx] )
         end
-      end
+      end 
     end
+    @children
   end
+
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  b = Board.new
+  test = TicTacToeNode.new(b, :x, nil)
+  p test.children
+
 end
